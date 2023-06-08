d668353dd387501cc9a25eabe4ae3d292f839e65
Abhay Kumar <kabhay166@gmail.com>
2023-06-08 19:36:38 +0530

    encapsulation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        /*
+        System.out.println(rwOnly.name = "ajay");
+        // error :- java: name has private access in com.driver.RWOnly
+         */
+
+        rwOnly.setName("Ajay");
+        System.out.println(rwOnly.getName());
+    }
 }
